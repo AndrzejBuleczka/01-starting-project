65e59176a6e38b5e704b5c7c7c99a15bfac96709
Andrzej Bułeczka <andrzej.buleczka@blacklabel.net>
2025-01-22 17:04:25 +0100

    added dialog for task addition and closing logic

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -1,11 +1,12 @@
 import { Component, Input } from '@angular/core';
 import { TaskComponent } from "./task/task.component";
 import { dummyTasks } from "./../dummy-tasks";
+import { NewTaskComponent } from "./new-task/new-task.component";
 
 @Component({
   selector: 'app-tasks',
   standalone: true,
-  imports: [TaskComponent],
+  imports: [TaskComponent, NewTaskComponent],
   templateUrl: './tasks.component.html',
   styleUrl: './tasks.component.css'
 })
@@ -13,6 +14,7 @@ export class TasksComponent {
   @Input({required: true}) userId!: string;
   @Input({required: true}) name!: string;
   tasks = dummyTasks
+  isAddingTask = false;
 
   get selectedUserTasks() {
     return this.tasks.filter(task => task.userId === this.userId);
@@ -20,6 +22,21 @@ export class TasksComponent {
 
   onCompleteTask(id: string) {
     this.tasks = this.tasks.filter(task => task.id !== id);
+  }
+
+  onStartAddTask() {
+    this.isAddingTask = true;
+    this.tasks.push({
+      id: Math.random().toString(),
+      userId: this.userId,
+      title: `Task ${this.tasks.length + 1}`,
+      summary: `Summary for task ${this.tasks.length + 1}`,
+      dueDate: new Date().toISOString(),
+    });
+  }
+
+  onCancelAddTask() {
+    this.isAddingTask = false;
   }
 
 }
