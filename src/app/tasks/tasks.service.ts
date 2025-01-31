7aab9476999ebfbe2d2684aef96adc3faf2b1b70
Andrzej Bułeczka <andrzej.buleczka@blacklabel.net>
2025-01-31 16:10:14 +0100

    added localStorage logic for tasks

--- src/app/tasks/tasks.service.ts
+++ src/app/tasks/tasks.service.ts
@@ -7,6 +7,14 @@ import { NewTaskData } from "./task/task.model";
 export class TasksService {
   private tasks = dummyTasks
 
+  constructor() {
+    const tasks = localStorage.getItem('tasks')
+    if (tasks) {
+      this.tasks = JSON.parse(tasks)
+    }
+    
+  }
+
   getUserTasks(userId: string) {
     return this.tasks.filter(task => task.userId === userId);
   }
@@ -18,10 +26,17 @@ export class TasksService {
       title: taskData.title,
       summary: taskData.summary,
       dueDate: taskData.date
-    })
+    });
+
+    this.saveTasks()
   }
 
   removeTask(id: string) {
     this.tasks = this.tasks.filter(task => task.id !== id)
+    this.saveTasks()
+  }
+
+  private saveTasks() {
+    localStorage.setItem('tasks', JSON.stringify(this.tasks))
   }
 }
